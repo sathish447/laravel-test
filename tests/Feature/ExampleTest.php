697b09b4082348535742bd80bf129e3842ea8519
sathish447 <sathishprabu447@gmail.com>
2024-10-02 22:23:46 +0530

    check webpage any contains is there are not

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -16,4 +16,13 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function test_the_homepage_cntains_response(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertSee('sathish');
+
+        $response->assertStatus(200);
+    }
 }
